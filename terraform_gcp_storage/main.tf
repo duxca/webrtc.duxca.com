9587ebb087fa82a6ff9b20d550901a61f552ed6b
Legokichi Duckscallion <legokichi@gmail.com>
2026-06-22 23:23:34 +0900

    Enable required GCP service APIs

--- terraform_gcp_storage/main.tf
+++ terraform_gcp_storage/main.tf
@@ -4,6 +4,16 @@ provider "google" {
   add_terraform_attribution_label = false
 }
 
+resource "google_project_service" "cloud_resource_manager_api" {
+  service            = "cloudresourcemanager.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "iam_api" {
+  service            = "iam.googleapis.com"
+  disable_on_destroy = false
+}
+
 resource "google_project_service" "cloud_run_api" {
   service            = "run.googleapis.com"
   disable_on_destroy = false
